2b5bee69c947f305f583cc6ea9050f34dd98b9b6
huangyanzu <709245577@qq.com>
2020-12-28 23:23:52 +0800

    初始化项目

---
+++ CMTV/CMTV/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  CMTV
+//
+//  Created by aZu on 2020/12/28.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow? 
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    
+        
+        UITabBar.appearance().tintColor = UIColor.orange 
+        
+        
+        
+        return true
+    }
+
+   
+
+
+}
+
